22d8e211d37aa4179bb0eed3d2a1c6fae02534e2
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-09-09 19:30:12 +0800

    added local index

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var _index = 'wiki2_de_2017_09_09';
 var _type = 'article';
 
 let client = new elasticsearch.Client({
-	host: 'connectionString',
+	host: connectionString,
 	log: 'trace'
 })
 
@@ -23,10 +23,11 @@ const App = React.createClass({
 		client.search({
                 index: _index,
                 type: _type,
+								q: search_query,
                 body: {
                   query: {
                       multi_match: {
-                          query: req.query.q,
+                          query: search_query,
                           fields: ['title^100', 'tags^100', 'abstract^20', 'description^10', 'chapter^5', 'title2^10', 'description2^10'],
                           fuzziness: 1,
                         },
