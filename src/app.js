7af87e1b1621dd193ec4b1d161fb8591f77652ad
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-09-09 19:03:40 +0800

    added index

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import elasticsearch from 'elasticsearch'
 
+var connectionString = 'localhost:9200';
+var _index = 'wiki2_de_2017_09_09';
+var _type = 'article';
+
 let client = new elasticsearch.Client({
-	host: 'localhost:9200',
+	host: 'connectionString',
 	log: 'trace'
 })
 
@@ -17,7 +21,17 @@ const App = React.createClass({
 		const search_query = event.target.value
 
 		client.search({
-			q: search_query
+                index: _index,
+                type: _type,
+                body: {
+                  query: {
+                      multi_match: {
+                          query: req.query.q,
+                          fields: ['title^100', 'tags^100', 'abstract^20', 'description^10', 'chapter^5', 'title2^10', 'description2^10'],
+                          fuzziness: 1,
+                        },
+                    },
+                },
 		}).then(function ( body ) {
 			this.setState({ results: body.hits.hits })
 		}.bind(this), function ( error ) {
